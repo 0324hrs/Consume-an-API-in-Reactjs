cf9ddfdeefdd2326dea8d1f69153c446a477b286
Maina Gacheru <mke445571@gmail.com>
2022-02-17 22:24:09 +0300

    a long day it was

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,7 +5,7 @@ import './Navbar.css'
 function Navbar(){
 
     return <div style={{backgroundColor:'#CCCCC3'}}>
-        <h1 style={{ color:'black' }}>Kinuthia Foods </h1>
+        <h1 style={{ color:'black' }}> El Classico Sea Dishes </h1>
         <a href='/' style={{padding:'10px',color:'black'}}>Home</a>
         <a href='/orders' style={{padding:'10px',color:'black'}}>Orders</a>
         <a href='/contacts' style={{padding:'10px',color:'black'}}>Contact us</a>
