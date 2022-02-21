18c148b7aa8380c892426ce31e819c12eb789b22
Maina Gacheru <mke445571@gmail.com>
2022-02-21 20:59:04 +0300

    react icons added

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,19 @@
 import React from "react";
 import './Navbar.css'
+import {FaHome} from 'react-icons/fa'
+import {AiFillContacts} from 'react-icons/ai'
+import {MdShoppingCart} from 'react-icons/md'
 
 
 function Navbar(){
 
     return <div style={{backgroundColor:'#CCCCC3'}}>
         <h1 style={{ color:'black' }}> El Classico Sea Dishes </h1>
-        <a href='/' style={{padding:'10px',color:'black'}}>Home</a>
-        <a href='/orders' style={{padding:'10px',color:'black'}}>Orders</a>
-        <a href='/contacts' style={{padding:'10px',color:'black'}}>Contact us</a>
+        
+        <a href='/' style={{padding:'10px',color:'black'}}><FaHome size='2rem'/></a>
+        <a href='/orders' style={{padding:'10px',color:'black'}}><MdShoppingCart size='2rem' /></a>
+        <a href='/contacts' style={{padding:'10px',color:'black'}}><AiFillContacts size='2rem' /></a>
+        
     </div>
 }
 
